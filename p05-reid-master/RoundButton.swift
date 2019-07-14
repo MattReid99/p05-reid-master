05180bad20a31264c11a10fc39d832b2a35b02d9
Matthew Reid <merknicks11@gmail.com>
2019-07-13 22:32:14 -0400

    finished

---
+++ p05-reid-master/RoundButton.swift
@@ -0,0 +1,40 @@
+//
+//  RoundButton.swift
+//  p04-reid-master
+//
+//  Created by Matthew Reid on 7/3/19.
+//  Copyright © 2019 Matthew Reid. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class RoundButton: UIButton {
+    
+    @IBInspectable var cornerRadius: CGFloat = 15 {
+        didSet {
+            refreshCorners(value: cornerRadius)
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        sharedInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        sharedInit()
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        sharedInit()
+    }
+    
+    func sharedInit() {
+        refreshCorners(value: cornerRadius)
+    }
+    
+    func refreshCorners(value: CGFloat) {
+        layer.cornerRadius = value
+    }
+}
--- p05-reid-master/ViewController.swift
+++ p05-reid-master/ViewController.swift
@@ -11,13 +11,18 @@ import UIKit
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
 
+    @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var tableView: UITableView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.delegate = self
         tableView.dataSource = self
+        
+        Globals.getData()
+        tableView.reloadData()
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -27,6 +32,8 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell.init(style: .default, reuseIdentifier: "cell")
         cell.textLabel?.text = "\(Globals.data[indexPath.row])"
+        cell.backgroundColor = view.backgroundColor!
+        cell.textLabel?.textColor = UIColor.white
         return cell
     }
     
@@ -37,14 +44,35 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if (editingStyle == .delete) {
             Globals.data.remove(at: indexPath.row)
+            Globals.saveData()
             self.tableView.reloadData()
         }
     }
     
     
+    @IBAction func addPressed(_ sender: Any) {
+        guard Int(textField.text!) != nil else {
+            return
+        }
+        let val = Int(textField.text!)!
+        Globals.data.append(val)
+        self.textField.text = ""
+        Globals.saveData()
+        self.tableView.reloadData()
+    }
     
     
-
-
+    
+    @IBAction func clearPressed(_ sender: Any) {
+        let alert = UIAlertController.init(title: "Are you sure you want to delete your data?", message: nil, preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction.init(title: "Yes", style: .default, handler: { Void in
+            Globals.data.removeAll()
+            Globals.saveData()
+            self.tableView.reloadData()
+        }))
+        alert.addAction(UIAlertAction.init(title: "Cancel", style: .cancel, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
 }
 
